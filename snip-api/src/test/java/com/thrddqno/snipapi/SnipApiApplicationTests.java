479e8ad75b83933de18c0fd0351e78f869e56f25
Antonio Dioquino <antonio.dioquino@hotmail.com>
2026-04-07 16:04:47 +0800

    initialize spring boot project and starters

---
+++ snip-api/src/test/java/com/thrddqno/snipapi/SnipApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thrddqno.snipapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnipApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
